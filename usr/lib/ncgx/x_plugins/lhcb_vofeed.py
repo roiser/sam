fb8e63aa89b936cbb12ca55dece546f63d2bc88d
Marian Babik <marian.babik@cern.ch>
2016-09-22 13:27:25 +0200

    adding support for HTCONDOR-CE and hostgroups/notifications

--- usr/lib/ncgx/x_plugins/lhcb_vofeed.py
+++ usr/lib/ncgx/x_plugins/lhcb_vofeed.py
@@ -1,38 +1,43 @@
 import logging
 
-from ncgx.inventory import Hosts, Checks
+from ncgx.inventory import Hosts, Checks, Groups
 from vofeed.api import VOFeed
 
 log = logging.getLogger('ncgx')
 
 WN_METRICS = (
-'emi.cream.glexec.WN-gLExec-/lhcb/Role=pilot',
-'org.lhcb.WN-cvmfs-/lhcb/Role=production',
-'org.lhcb.WN-mjf-/lhcb/Role=production',
-'org.lhcb.WN-sft-csh-/lhcb/Role=production',
-'org.lhcb.WN-sft-lcg-rm-gfal-/lhcb/Role=production',
-'org.lhcb.WN-sft-vo-swdir-/lhcb/Role=production',
-'org.lhcb.WN-sft-voms-/lhcb/Role=production'
+    'emi.cream.glexec.WN-gLExec-/lhcb/Role=pilot',
+    'org.lhcb.WN-cvmfs-/lhcb/Role=production',
+    'org.lhcb.WN-mjf-/lhcb/Role=production',
+    'org.lhcb.WN-sft-csh-/lhcb/Role=production',
+    'org.lhcb.WN-sft-lcg-rm-gfal-/lhcb/Role=production',
+    'org.lhcb.WN-sft-vo-swdir-/lhcb/Role=production',
+    'org.lhcb.WN-sft-voms-/lhcb/Role=production'
 )
 
 ARC_METRICS = (
-'org.sam.ARC-JobSubmit-/lhcb/Role=pilot',
-'org.sam.ARC-JobSubmit-/lhcb/Role=production'
+    'org.sam.ARC-JobSubmit-/lhcb/Role=pilot',
+    'org.sam.ARC-JobSubmit-/lhcb/Role=production'
 )
 
 CREAM_METRICS = (
-'emi.cream.glexec.CREAMCE-DirectJobSubmit-/lhcb/Role=pilot',
-'emi.cream.CREAMCE-DirectJobSubmit-/lhcb/Role=production'
+    'emi.cream.glexec.CREAMCE-DirectJobSubmit-/lhcb/Role=pilot',
+    'emi.cream.CREAMCE-DirectJobSubmit-/lhcb/Role=production'
+)
+
+CONDOR_METRICS = (
+    'org.sam.CONDOR-JobSubmit-/lhcb/Role=pilot',
+    'org.sam.CONDOR-JobSubmit-/lhcb/Role=production'
 )
 
 SRM_METRICS = (
-'org.lhcb.SRM-AllLHCb-/lhcb/Role=production',
-'org.lhcb.SRM-GetLHCbInfo-/lhcb/Role=production',
-'org.lhcb.SRM-VODel-/lhcb/Role=production',
-'org.lhcb.SRM-VOGet-/lhcb/Role=production',
-'org.lhcb.SRM-VOLs-/lhcb/Role=production',
-'org.lhcb.SRM-VOLsDir-/lhcb/Role=production',
-'org.lhcb.SRM-VOPut-/lhcb/Role=production')
+    'org.lhcb.SRM-AllLHCb-/lhcb/Role=production',
+    'org.lhcb.SRM-GetLHCbInfo-/lhcb/Role=production',
+    'org.lhcb.SRM-VODel-/lhcb/Role=production',
+    'org.lhcb.SRM-VOGet-/lhcb/Role=production',
+    'org.lhcb.SRM-VOLs-/lhcb/Role=production',
+    'org.lhcb.SRM-VOLsDir-/lhcb/Role=production',
+    'org.lhcb.SRM-VOPut-/lhcb/Role=production')
 
 
 def run(url):
@@ -40,7 +45,8 @@ def run(url):
 
     # Get services from the VO feed, i.e 
     # list of tuples (hostname, flavor, endpoint)
-    services = VOFeed(url).get_services()
+    feed = VOFeed(url)
+    services = feed.get_services()
     
     # Add hosts, each tagged with corresponding flavors
     # creates /etc/ncgx/conf.d/generated_hosts.cfg
@@ -52,9 +58,18 @@ def run(url):
     # Add corresponding metrics to tags
     # creates /etc/ncgx/conf.d/generated_checks.cfg
     c = Checks()
-    c.add_all(CREAM_METRICS, tags=["CREAM-CE",])
-    c.add_all(ARC_METRICS, tags=["ARC-CE",])
+    c.add_all(CREAM_METRICS, tags=["CREAM-CE", ])
+    c.add_all(ARC_METRICS, tags=["ARC-CE", ])
+    c.add_all(CONDOR_METRICS, tags=["HTCONDOR-CE", ])
     c.add_all(WN_METRICS, tags=["ARC-CE", "CREAM-CE"])
-    c.add_all(SRM_METRICS, tags=["SRMv2",])
+    c.add_all(SRM_METRICS, tags=["SRMv2", ])
     c.serialize()
+
+    # Add host groups
+    sites = feed.get_groups("LHCb_Site")
+    hg = Groups("host_groups")
+    for site, hosts in sites.iteritems():
+        for host in hosts:
+            hg.add(site, host)
+    hg.serialize()
 
